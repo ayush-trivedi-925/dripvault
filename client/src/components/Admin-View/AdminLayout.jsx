c4c2774afda64c06da5e5122a096c66dff659cca
ayush-trivedi-925 <asxu525@gmail.com>
2025-02-18 20:33:01 +0530

    Completed admin page layout consisting :- Admin sidebar and Admin header

--- client/src/components/Admin-View/AdminLayout.jsx
+++ client/src/components/Admin-View/AdminLayout.jsx
@@ -1,15 +1,17 @@
 import { Outlet } from "react-router-dom";
 import AdminSideBar from "./AdminSideBar";
 import AdminHeader from "./AdminHeader";
+import { useState } from "react";
 
 export default function AdminLayout() {
+  const [openSidebar, setOpenSidebar] = useState(false);
   return (
     <div className="flex min-h-screen w-full ">
       {/* Admin Sidebar */}
-      <AdminSideBar />
+      <AdminSideBar open={openSidebar} setOpen={setOpenSidebar} />
       <div className="flex flex-1 flex-col ">
         {/* Admin Header */}
-        <AdminHeader />
+        <AdminHeader setOpen={setOpenSidebar} />
         <main className="flex-1 flex bg-muted/40 p-4 md:p-6">
           <Outlet />
         </main>
--- client/src/components/Admin-View/AdminSideBar.jsx
+++ client/src/components/Admin-View/AdminSideBar.jsx
@@ -1,3 +1,96 @@
-export default function AdminSideBar() {
-  return <div>Admin SideBar</div>;
+import { ShieldCheck } from "lucide-react";
+import { useNavigate } from "react-router-dom";
+import {
+  CircleEllipsis,
+  LayoutDashboard,
+  Package,
+  ShoppingCart,
+} from "lucide-react";
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+} from "../ui/sheet";
+
+const adminSidebarMenuItems = [
+  {
+    id: "dashboard",
+    label: "Dashboard",
+    path: "/admin/dashboard",
+    icon: <LayoutDashboard />,
+  },
+  {
+    id: "products",
+    label: "Products",
+    path: "/admin/products",
+    icon: <Package />,
+  },
+  {
+    id: "orders",
+    label: "Orders",
+    path: "/admin/orders",
+    icon: <ShoppingCart />,
+  },
+  {
+    id: "features",
+    label: "Features",
+    path: "/admin/features",
+    icon: <CircleEllipsis />,
+  },
+];
+
+function MenuItems({ setOpen }) {
+  const navigate = useNavigate();
+  return (
+    <nav className="mt-8 flex flex-col gap-2">
+      {adminSidebarMenuItems.map((menuItem) => {
+        return (
+          <div
+            className="flex items-center cursor-pointer text-xl gap-2 rounded-md px-3 py-2 text-muted-foreground hover:bg-muted hover:text-foreground"
+            key={menuItem.id}
+            onClick={() => {
+              navigate(menuItem.path);
+              setOpen ? setOpen(false) : null;
+            }}
+          >
+            {menuItem.icon}
+            <span>{menuItem.label}</span>
+          </div>
+        );
+      })}
+    </nav>
+  );
+}
+
+export default function AdminSideBar({ open, setOpen }) {
+  const navigate = useNavigate();
+  return (
+    <>
+      <Sheet open={open} onOpenChange={setOpen}>
+        <SheetContent side="left" className="w-64">
+          <div className="flex flex-col h-full">
+            <SheetHeader className="border-b">
+              <SheetTitle className="flex gap-2 mt-5 mb-5">
+                <ShieldCheck size={30} />
+                <span>Admin Panel</span>
+              </SheetTitle>
+            </SheetHeader>
+            <MenuItems setOpen={setOpen} />
+          </div>
+        </SheetContent>
+      </Sheet>
+      <aside className="hidden w-64 flex-col border-r bg-background p-6 lg:flex ">
+        <div
+          className="flex cursor-pointer items-center gap-2"
+          onClick={() => navigate("/admin/dashboard")}
+        >
+          <ShieldCheck size={30} />
+          <h1 className="text-xl font-extrabold ">Admin Panel</h1>
+        </div>
+        <MenuItems setOpen={setOpen} />
+      </aside>
+    </>
+  );
 }
