f6ee4dfe25acc1407ae56764ac6acc5fc8466c89
ayush-trivedi-925 <asxu525@gmail.com>
2025-02-20 19:16:22 +0530

    Added logout functionality

--- client/src/store/auth-slice/index.js
+++ client/src/store/auth-slice/index.js
@@ -32,6 +32,18 @@ export const loginUser = createAsyncThunk("/auth/login", async (formData) => {
   return response.data;
 });
 
+export const logoutUser = createAsyncThunk("/auth/logout", async () => {
+  const response = await axios.post(
+    "http://localhost:5000/api/auth/logout",
+    {},
+
+    {
+      withCredentials: true,
+    }
+  );
+  return response.data;
+});
+
 export const checkAuth = createAsyncThunk("/auth/checkauth", async () => {
   const response = await axios.get(
     "http://localhost:5000/api/auth/checkauth",
@@ -90,6 +102,11 @@ const authSlice = createSlice({
         state.isAuthenticated = action.payload.success ? true : false;
       })
       .addCase(checkAuth.rejected, (state, action) => {
+        state.isLoading = false;
+        state.user = null;
+        state.isAuthenticated = false;
+      })
+      .addCase(logoutUser.fulfilled, (state) => {
         state.isLoading = false;
         state.user = null;
         state.isAuthenticated = false;
